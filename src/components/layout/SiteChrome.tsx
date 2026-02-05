79c8443d7846859a4c8a602ee37e591fbbe055e3
Adi Jain <adi1510jain@gmail.com>
2026-02-06 02:01:16 +0530

    enhanced designs

--- src/components/layout/SiteChrome.tsx
+++ src/components/layout/SiteChrome.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import Header from "./Header";
+// import Header from "./Header";
 import Navbar from "./Navbar";
 import FullscreenMenu from "./FullscreenMenu";
 import SearchModal from "./SearchModal";
